c0f953fccebc85cdfe735f8029280bc9b58f8b2f
Gabriel <gabriel06tavares29@gmail.com>
2023-05-07 15:45:15 -0300

    Menu principal

--- Trabalho/src/main/java/com/mycompany/trabalho/Trabalho.java
+++ Trabalho/src/main/java/com/mycompany/trabalho/Trabalho.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.trabalho;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Davi
@@ -11,6 +13,54 @@ package com.mycompany.trabalho;
 public class Trabalho {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bem vindo ao sistema de competições!");
+        
+        int opcao = 0;
+        while (opcao != 5) {
+            do {
+                System.out.println("Menu principal:");
+                System.out.println(" 1) Cadastrar Equipe");
+                System.out.println(" 2) Cadastrar Robô");
+                System.out.println(" 3) Cadastrar Competidor");
+                System.out.println(" 4) Cadastrar Categoria");            
+                System.out.println(" 5) Sair");
+                System.out.println("Opção: ");
+                opcao = input.nextInt();
+            } while (opcao < 1 || opcao > 4);
+
+
+            switch (opcao) {
+                case 1 -> {
+                    CadastrarEquipe();
+                }
+                case 2 -> {
+                    CadastrarRobo();
+                }
+                case 3 -> {
+                    CadastrarCompetidor();                
+                }
+                case 4 -> {
+                    CadastrarCategoria();                
+                }
+                case 5 -> {
+                    System.out.println("Obrigado por usar nosso sistema!");
+                }
+            }         
+        }
     }
+    
+    public static void CadastrarEquipe(){
+        
+    }
+    public static void CadastrarRobo(){
+        
+    }
+    public static void CadastrarCompetidor(){
+        
+    }
+    public static void CadastrarCategoria(){
+        
+    }
+    
 }
